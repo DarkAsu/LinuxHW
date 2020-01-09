395d3ede325913c093cb7615d860e1377d66c83d
DarkAsu <694237885@qq.com>
2020-01-09 11:04:00 +0800

    http.h version 1.0

---
+++ http.h
@@ -0,0 +1,30 @@
+#ifndef _HTTP_
+#define _HTTP_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/sendfile.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <pthread.h>
+#include <assert.h>
+#include <string.h>
+#include <fcntl.h>
+
+#define NORMAL 0
+#define WARNING 1
+#define FATAL 2
+
+#define SIZE 4096
+
+void Usage(const char* proc);
+void print_log(const char* log_msg, int level);
+
+int handle(int fd);
+void echo_errno();
+
+#endif
